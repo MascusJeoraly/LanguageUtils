9f3d8760bfffe7621ba018fa4084f10a6ddf1ed3
Meow J <fang0716fy@gmail.com>
2015-06-22 15:49:41 +0800

    Fix compatibility with JUnit 4.10 Add IDEA support Add Eclipse support(not tested yet)

--- src/test/java/com/meowj/langutils/lang/convert/ItemEntryTest.java
+++ src/test/java/com/meowj/langutils/lang/convert/ItemEntryTest.java
@@ -16,6 +16,8 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.*;
 
 public class ItemEntryTest {
@@ -26,36 +28,36 @@ public class ItemEntryTest {
         ItemEntry equalEntry1 = new ItemEntry(Material.STONE);
         ItemEntry equalEntry2 = new ItemEntry(Material.STONE);
 
-        assertEquals(equalEntry1, equalEntry2);
-        assertEquals(equalEntry1.hashCode(), equalEntry2.hashCode());
+        assertThat(equalEntry1, is(equalEntry2));
+        assertThat(equalEntry1.hashCode(), is(equalEntry2.hashCode()));
 
         //Same Material + metadata
         ItemEntry equalEntry3 = new ItemEntry(Material.STONE, 2);
         ItemEntry equalEntry4 = new ItemEntry(Material.STONE, 2);
 
-        assertEquals(equalEntry3, equalEntry4);
-        assertEquals(equalEntry3.hashCode(), equalEntry4.hashCode());
+        assertThat(equalEntry3, is(equalEntry4));
+        assertThat(equalEntry3.hashCode(), is(equalEntry4.hashCode()));
 
         //Different Material
         ItemEntry diffEntry1 = new ItemEntry(Material.STONE);
         ItemEntry diffEntry2 = new ItemEntry(Material.ANVIL);
 
-        assertNotEquals(diffEntry1, diffEntry2);
-        assertNotEquals(diffEntry1.hashCode(), diffEntry2.hashCode());
+        assertThat(diffEntry1, not(diffEntry2));
+        assertThat(diffEntry1.hashCode(), not(diffEntry2.hashCode()));
 
         //Different metadata
         ItemEntry diffEntry3 = new ItemEntry(Material.STONE, 4);
         ItemEntry diffEntry4 = new ItemEntry(Material.STONE, 7);
 
-        assertNotEquals(diffEntry3, diffEntry4);
-        assertNotEquals(diffEntry3.hashCode(), diffEntry4.hashCode());
+        assertThat(diffEntry3, not(diffEntry4));
+        assertThat(diffEntry3.hashCode(), not(diffEntry4.hashCode()));
 
         //Different Material + metadata
         ItemEntry diffEntry5 = new ItemEntry(Material.STONE, 4);
         ItemEntry diffEntry6 = new ItemEntry(Material.SAND, 2);
 
-        assertNotEquals(diffEntry5, diffEntry6);
-        assertNotEquals(diffEntry5.hashCode(), diffEntry6.hashCode());
+        assertThat(diffEntry5, not(diffEntry6));
+        assertThat(diffEntry5.hashCode(), not(diffEntry6.hashCode()));
 
 
         //HashMap Test
