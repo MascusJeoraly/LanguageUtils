b0ce921c181e82ab71ed9b9249b4b2bb26f4ec4e
Meow J <fang0716fy@gmail.com>
2015-06-29 19:10:28 +0800

    Update some items

--- src/main/java/com/meowj/langutils/lang/convert/EnumItem.java
+++ src/main/java/com/meowj/langutils/lang/convert/EnumItem.java
@@ -27,15 +27,32 @@ import java.util.Map;
 public enum EnumItem {
 
     AIR(Material.AIR, "Air"),
-    //Stone
     STONE(Material.STONE, "tile.stone.stone.name"),
     GRANITE(Material.STONE, 1, "tile.stone.granite.name"),
     POLISHED_GRANITE(Material.STONE, 2, "tile.stone.graniteSmooth.name"),
     DIORITE(Material.STONE, 3, "tile.stone.diorite.name"),
     POLISHED_DIORITE(Material.STONE, 4, "tile.stone.dioriteSmooth.name"),
     ANDESITE(Material.STONE, 5, "tile.stone.andesite.name"),
-    POLISHED_ANDESITE(Material.STONE, 6, "tile.stone.andesiteSmooth.name");
-    //WIP Lazy, who can help me? (;´Д`)
+    POLISHED_ANDESITE(Material.STONE, 6, "tile.stone.andesiteSmooth.name"),
+    BARRIER(Material.BARRIER, "tile.barrier.name"),
+    HAY_BALE(Material.HAY_BLOCK, "tile.hayBlock.name"),
+    GRASS_BLOCK(Material.GRASS, "tile.grass.name"),
+    DIRT(Material.DIRT, 0, "tile.dirt.default.name"),
+    COARSE_DIRT(Material.DIRT, 1, "tile.dirt.coarse.name"),
+    PODZOL(Material.DIRT, 2, "tile.dirt.podzol.name"),
+    COBBLESTONE(Material.COBBLESTONE, "tile.stonebrick.name"),
+    OAK_WOOD_PLANKS(Material.WOOD, "tile.wood.oak.name"),
+    SPRUCE_WOOD_PLANKS(Material.WOOD, 1, "tile.wood.spruce.name"),
+    BIRCH_WOOD_PLANKS(Material.WOOD, 2, "tile.wood.birch.name"),
+    JUNGLE_WOOD_PLANKS(Material.WOOD, 3, "tile.wood.jungle.name"),
+    ACACIA_WOOD_PLANKS(Material.WOOD, 4, "tile.wood.acacia.name"),
+    DARK_OAK_WOOD_PLANKS(Material.WOOD, 5, "tile.wood.big_oak.name"),
+    OAK_SAPLING(Material.SAPLING, "tile.sapling.oak.name"),
+    SPRUCE_SAPLING(Material.SAPLING, 1, "tile.sapling.spruce.name"),
+    BIRCH_SAPLING(Material.SAPLING, 2, "tile.sapling.birch.name"),
+    JUNGLE_SAPLING(Material.SAPLING, 3, "tile.sapling.jungle.name"),
+    ACACIA_SAPLING(Material.SAPLING, 4, "tile.sapling.acacia.name"),
+    DARK_OAK_SAPLING(Material.SAPLING, 5, "tile.sapling.big_oak.name");
 
 
     private Material material;
